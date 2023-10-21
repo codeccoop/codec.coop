ecad503281e4953069f9bbaf0c25bd366cc6a630
Orzo Cogorzo <orzocogorzo@hotmail.com>
2023-10-21 13:27:45 +0200

    feat: products badge interactivity

--- assets/js/pages/products.js
+++ assets/js/pages/products.js
@@ -1,43 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
   var $el = document.getElementsByTagName("main")[0];
 
-  var description;
-  var descriptions = document.getElementsByClassName(
-    "products__product-toggle"
-  );
-  for (var i = 0; i < descriptions.length; i++) {
-    function toggleVisibility(ev) {
-      if (ev.target.classList.contains("products__product-contact")) {
-        window.scrollBy(
-          0,
-          document.getElementById("pageFooter").getBoundingClientRect().top
-        );
-      } else {
-        if (!ev.target.classList.contains("products__product-description")) {
-          ev.currentTarget.classList.toggle("open");
-          if (ev.currentTarget.classList.contains("open")) {
-            ev.currentTarget.setAttribute("aria-pressed", "true");
-            ev.currentTarget.setAttribute("aria-expanded", "true");
-          } else {
-            ev.currentTarget.setAttribute("aria-pressed", "false");
-            ev.currentTarget.setAttribute("aria-expanded", "false");
-          }
+  function bindToEnter(fn, keyCode) {
+    return function (ev) {
+      if (ev.keyCode === keyCode) {
+        fn(ev);
+      }
+    };
+  }
+
+  function toggleVisibility(ev) {
+    if (ev.target.classList.contains("products__product-contact")) {
+      window.scrollBy(
+        0,
+        document.getElementById("pageFooter").getBoundingClientRect().top
+      );
+    } else {
+      if (!ev.target.classList.contains("products__product-description")) {
+        ev.currentTarget.classList.toggle("open");
+        if (ev.currentTarget.classList.contains("open")) {
+          ev.currentTarget.setAttribute("aria-pressed", "true");
+          ev.currentTarget.setAttribute("aria-expanded", "true");
+        } else {
+          ev.currentTarget.setAttribute("aria-pressed", "false");
+          ev.currentTarget.setAttribute("aria-expanded", "false");
         }
       }
     }
+  }
 
-    function bindToEnter(fn, keyCode) {
-      return function (ev) {
-        if (ev.keyCode === keyCode) {
-          fn(ev);
-        }
-      };
-    }
-
-    descriptions[i].addEventListener("click", toggleVisibility);
-    descriptions[i].addEventListener(
-      "keydown",
-      bindToEnter(toggleVisibility, 13)
+  var products = $el.getElementsByClassName("products__product");
+  for (var i = 0; i < products.length; i++) {
+    const thumbnail = products[i].querySelector(".products__product-badge");
+    const description = products[i].querySelector(".products__product-toggle");
+    thumbnail.addEventListener("click", () =>
+      description.dispatchEvent(new Event("click"))
     );
+    description.addEventListener("click", toggleVisibility);
+    description.addEventListener("keydown", bindToEnter(toggleVisibility, 13));
   }
 });
